8032a4d39aa891b5a4fe5d0aa33cc0fdc98bbcc2
Santiago Torres G <thdealer@gmail.com>
2019-02-20 14:08:04 -0500

    adding transposition solution

---
+++ Ruby/transposition.rb
@@ -0,0 +1,32 @@
+def transpose(string)
+  copy = string
+  output = string
+  idx = 0
+
+  while !idx.nil? do
+    idx = output.index(/g{1,}n{1,}/, 0)
+    if output.match(/g{1,}n{1,}/)
+      asset = output[idx, output.length].match(/n{1,}/)
+      basset = output[idx, output.length].match(/g{1,}/)
+      output = output[0, idx] + asset.to_s + basset.to_s
+      num = output.length
+      copy[0, num] = output
+      output = copy
+    end
+  end
+  output
+end
+
+# MODEL SOLUTION BY DANIEL SANCHEZ 
+
+# def transpose(string)
+#   while string.include?('gn')
+# 		string.gsub!('gn', 'ng')
+# 	end
+	
+# 	string
+# end
+
+puts transpose('rigggnadigndiggn!')
+puts transpose('nggngngnggng')
+puts transpose('imsignining')
