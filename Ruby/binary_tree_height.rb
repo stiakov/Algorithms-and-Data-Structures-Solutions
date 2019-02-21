610e92e4180415247fe0d4ac355aa6bee999ad6f
Santiago Torres G <thdealer@gmail.com>
2019-02-21 16:30:16 -0500

    adding binary tree height measurer and refactoring transposition

---
+++ Ruby/binary_tree_height.rb
@@ -0,0 +1,49 @@
+def tree_height(tree_as_list)
+  step = 1
+  max = 1
+  ctr = 0
+
+  if tree_as_list.empty?
+    max = 0
+  else
+    until ctr.nil?
+      ctr = tree_as_list[(step * 2) + 1]
+      unless ctr.nil?
+        step *= 2
+        max += 1
+      end
+    end
+  end
+
+  max
+end
+
+# def tree_height(tree_as_list)
+#   step = 1
+#   if tree_as_list.empty?
+#     max = 0
+#   else
+#     r_height = 0
+#     l_height = 0
+#     max = 0
+
+#     until step.nil?
+
+#       l_height = !tree_as_list[(step * 2) + 1].nil? ? l_height + 1 : l_height
+#       r_height = !tree_as_list[(step * 2) + 2].nil? ? r_height + 1 : r_height
+#       l_step_ctr = tree_as_list[(step * 2) + 1]
+#       r_step_ctr = tree_as_list[(step * 2) + 2]
+
+#       step = !l_step_ctr.nil? && !r_step_ctr.nil? ? step *= 2 : nil
+#     end
+#     max = l_height > r_height ? l_height : r_height
+#   end
+
+#   max + 1
+# end
+
+puts tree_height([2, 7, 5, 2, 6, 0, 9]) # => 3
+puts tree_height([10, 20, 10, 30, 20, 40, 20, 60]) # => 3
+puts tree_height([1, 7, 5, 2, 6, 0, 9, 3, 7, 5, 11, 0, 0, 4, 0]) # => 4
+puts tree_height([5, 3, 2, 9, 0, 0, 7, 0, 0, 0, 0, 0, 0, 5, 0]) # => 4
+puts 'end'
--- Ruby/transposition.rb
+++ Ruby/transposition.rb
@@ -7,8 +7,8 @@ def transpose(string)
     idx = output.index(/g{1,}n{1,}/, 0)
     if output.match(/g{1,}n{1,}/)
       asset = output[idx, output.length].match(/n{1,}/)
-      basset = output[idx, output.length].match(/g{1,}/)
-      output = output[0, idx] + asset.to_s + basset.to_s
+      asset += output[idx, output.length].match(/g{1,}/)
+      output = output[0, idx] + asset.to_s
       num = output.length
       copy[0, num] = output
       output = copy
