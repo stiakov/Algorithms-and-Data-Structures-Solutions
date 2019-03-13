9bc4e887fe52576b21fe1b299e9bd1e67343a7cf
Santiago Torres G <thdealer@gmail.com>
2019-03-13 15:00:26 -0500

    adding Towers of Hanoi recursive and leftmost_node_sum

--- Ruby/Towers of Hanoi recursive.rb
+++ Ruby/Towers of Hanoi recursive.rb
@@ -36,3 +36,29 @@ hanoi_steps(4)
 #    1->2
 #    1->3
 #    2->3
+
+
+=begin
+  
+MODEL SOLUTION
+
+def hanoi_steps(number_of_discs)
+  # your code here
+  step(number_of_discs, 1, 2, 3)
+end
+
+def step(number_of_discs, starting, intermediate, goal)
+  if number_of_discs == 1
+    puts move(starting, goal)
+  else
+    step(number_of_discs-1, starting, goal, intermediate)
+    puts move(starting, goal)
+    step(number_of_discs-1, intermediate, starting, goal)
+  end
+end
+
+def move(starting, goal)
+  "#{starting}->#{goal}"
+end
+  
+=end
--- Ruby/Trees/leftmost_node_sum.rb
+++ Ruby/Trees/leftmost_node_sum.rb
@@ -14,3 +14,24 @@ end
 puts leftmost_nodes_sum([5, 3, 4, 11, 13, 15, 21, 10, 4, 7, 2, 8, 0, 9, 0]) # => 29
 puts leftmost_nodes_sum([1, 7, 5, 2, 6, 0, 9, 3, 7, 5, 11, 0, 0, 4, 0]) # => 13
 puts leftmost_nodes_sum([2, 7, 5, 2, 6, 0, 9]) # => 11
+
+
+=begin
+MODEL SOLUTION
+
+def leftmost_nodes_sum(array)
+  # your code here
+  i = 0
+  sum = 0
+  exp = 2 ** i - 1
+  
+  while exp < array.size
+    sum += array[exp]
+    i = i + 1
+
+    exp = 2 ** i - 1
+  end
+  sum
+end
+
+=end
