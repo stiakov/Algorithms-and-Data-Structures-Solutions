7e86680124e893ba5f42e627db7067364ffaf4a7
Santiago Torres G <thdealer@gmail.com>
2019-03-15 11:56:43 -0500

    addng tree traversal solution

---
+++ Ruby/Trees/tree_traversal.rb
@@ -0,0 +1,57 @@
+class Node
+  attr_reader :data
+  attr_accessor :left, :right
+
+  def initialize(data)
+    @data = data
+  end
+end
+
+def array_to_tree(array, i)
+  return nil if i >= array.length || array[i] == 0
+
+  node = Node.new(array[i])
+  node.left = array_to_tree(array, 2 * i + 1)
+  node.right = array_to_tree(array, 2 * i + 2)
+
+  node
+end
+
+def pre_order(node)
+  return '' if node.nil?
+
+  result = "#{node.data} "
+  result += pre_order(node.left)
+  result += pre_order(node.right)
+end
+
+def post_order(node)
+  return '' if node.nil?
+
+  str = node.data.to_s
+  str = "#{post_order(node.right)} #{str}".strip
+  str = "#{post_order(node.left)} #{str}".strip
+end
+
+tree = array_to_tree([10, 1, 2, 3, 4, 5, 6], 0)
+puts pre_order(tree)
+puts post_order(tree)
+#=> 3 4 1 5 6 2 10
+
+=begin
+
+========= MODEL SOLUTION ================
+
+
+  def post_order(node)
+  # your code here
+  if node == nil
+    return ''
+  end
+
+  result = post_order(node.left)
+  result += post_order(node.right)
+  result += "#{node.data} "
+end
+
+=end
