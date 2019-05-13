039ff9e510430ec27827b8df0016d2faedfb23fb
Santiago Torres G <thdealer@gmail.com>
2019-05-13 11:37:35 -0500

    adding find_pairs in Maps dir

--- Ruby/Maps/find_pairs.rb
+++ Ruby/Maps/find_pairs.rb
@@ -16,3 +16,17 @@ p find_pairs([-8, 7, 11, 8, 5, 9, 3, 6, 2, -9, 4], 0)
 
 p find_pairs([1, 9, 11, 13, 2, 3, 7], 12)
 # => [[1, 11], [9, 3]]
+
+# # ========== MODEL SOLUTION ==========
+# def find_pairs(array, k)
+#   # write your code here
+#   pairs = []
+#
+#   array.each_with_index do |x, index|
+#     array[(index + 1)..-1].each do |y|
+#       pairs.push([x, y]) if x + y == k
+#     end
+#   end
+#
+#   pairs
+# end
