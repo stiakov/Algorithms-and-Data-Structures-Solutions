2dbb77d67be8ee3cba5cb94eb74b54b8aa7b16d1
Santiago Torres G <thdealer@gmail.com>
2019-07-19 13:14:27 -0500

    adding merge_sort_1.rb and .gitignore

--- Ruby/Sorting_Challenges/merge_sort_1.rb
+++ Ruby/Sorting_Challenges/merge_sort_1.rb
@@ -1,34 +1,26 @@
-def merge_sort(array1, array2)
-  merge_in(array1, array2)
-
-end
-
-def merge_in (ar1, ar2)
+def merge_sort(ar1, ar2)
   output = []
   len = ar1.size
   i, j = 0, 0
 
-  loop do
-    break if i > len || j > len
-
-    if i < len && j < len
-
-      if j < len && ar1[i] > ar2[j]
-        output << ar2[j]
-        j += 1
-      else
-        output << ar1[i]
-        i += 1
-      end
-
+  while i < len && j < len do
+    if ar1[i] > ar2[j]
+      output << ar2[j]
+      j += 1
     else
-        break
+      output << ar1[i]
+      i += 1
     end
   end
+
+  output += ar1[i..-1] if i < len
+  output += ar2[j..-1] if j < len
 
   output
 end
 
+p merge_sort([21, 32, 45, 62, 71, 83], [1, 3, 4, 7, 9, 12])
+# [1, 3, 4, 7, 9, 12, 21, 32, 45, 62, 71, 83]
 p merge_sort([-3, 2, 2, 2, 4], [-2, 0, 1, 5, 7])
 # [-3, -2, 0, 1, 2, 2, 2, 4, 5, 7]
 p merge_sort([1, 3, 9, 11], [2, 4, 6, 8])
